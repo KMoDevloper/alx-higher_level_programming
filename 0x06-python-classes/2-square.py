cfc8b9ba4ccaf4d68553f6a45ee9dbaea76158fd
KMoDevloper <mouaouiyakawtar@gmail.com>
2024-01-23 14:17:52 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,26 +12,9 @@ class Square:
             size (int): The size of the square (default is 0).
         """
         self.__size = size
-    @property
-    def size(self):
-        """Getter method for size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Setter method for zise.
-
-        Args:
-            Value (int): The size to set.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
-        self.__validate_size(value)
-        self.__size = value
+        self.__validate_size()
     
-    def __validate_size(self, value):
+    def __validate_size(self):
         """Validate the size attribute."""
         if not isinstance(self.__size, int):
             raise TypeError("Size must be an integer")
