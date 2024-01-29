9d9bd23df1638d3dd9c46b8611553d7f7d3f4085
KMoDevloper <mouaouiyakawtar@gmail.com>
2024-01-29 06:56:52 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,31 +11,34 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """Set the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be  >= 0")
-        self.__width = value
+        self.width = width
 
     @property
     def height(self):
-        """Set the height of the rectangle."""
+        """Getter method for height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        """Setter method for height"""
+        self.validate_non_negative_integer("width", value)
         self.__height = value
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        self.validate_non_negative_integer("width", value)
+        self.__width = value
+
+    def validate_non_negative_integer(self, attribute, value):
+        """Validate that the given value is a non-negative integer."""
+        if not isinstance(value, int):
+            raise TypeError(f"{attribute} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
