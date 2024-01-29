e0c80d02c1f8588cd2902c1799a142bf88e2cf6a
KMoDevloper <mouaouiyakawtar@gmail.com>
2024-01-29 11:26:49 -0800

    2-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a Rectangle class """
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def__init__(self, width=0, height=0):
+        """Initialize a new REctangle.
+
+        Args:
+           width (int): The width of the new rectangle.
+           height (int): The height of the new rectangle
+           """
+           self.width = width
+           self.height = height
+
+
