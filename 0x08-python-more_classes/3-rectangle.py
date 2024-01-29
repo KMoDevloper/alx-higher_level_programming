66c5125a395e651944b4cf9ea2545e70fe2fd3d8
KMoDevloper <mouaouiyakawtar@gmail.com>
2024-01-29 14:26:20 -0800

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,9 +50,12 @@ class Rectangle:
 
     def __str__(self):
         """Return a string representation of the rectangle"""
-        return "<Rectangle({}, {}) object at {}>".format(
-                self.__width, self.__height, hex(id(self)))
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width
+                          for _ in range(self.__height)])
 
     def __repr__(self):
         """Return a string representation of the rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(
+                self.__width, self.__height, hex(id(self)))
