f59aa5fe30d9c3f7d3e1041de8556527342886ca
KMoDevloper <mouaouiyakawtar@gmail.com>
2024-03-03 13:27:42 -0800

    Push All

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains a function that divides all elements of a matrix.
-
-:param a: The first integer.
-:param b: The second integer (default is 98).
-:return: The sum of a and b.
-:raises TypeErroor: If a or b is not an integer or float.
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """
-    Add two integers.
+    """Return the integer addition of a and b.
 
-    :param a: The first integer.
-    :param b: The second integer (default is 98).
-    :return: The sum of a and b.
-    :raises TypeError: If a or b is not an integer or float.
-    """
+    Float arguments are typecasted to ints before addition is performed.
 
-    if type(a) not in (int, float):
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-
-    if type(b) not in (int, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    return (int(a) + int(b))
